d95a02bfb27969656e813596f19fd19d93f2a61a
Jack Sims <123803257+sym-z@users.noreply.github.com>
2024-04-26 22:32:19 -0700

    End of night push

---
+++ Scripts/level_one_intro.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"Window Frame/Customer 1/Textbox".animation = "greeting"
+	$"Window Frame/Customer 1/Textbox".play()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
